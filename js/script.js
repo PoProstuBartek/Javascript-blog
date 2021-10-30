053b2fab214ed8cf3447ad34e18a46b0a9d0985b
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2021-10-30 15:14:43 +0200

    Add function that removes class active from links and articles

--- js/script.js
+++ js/script.js
@@ -10,25 +10,37 @@ function titleClickHandler(){
   
   buttonTest.addEventListener('click', titleClickHandler); */
   
-  function titleClickHandler(event){
+function titleClickHandler(event){
     console.log('Link was clicked!');
     console.log(event);
-  }
+}
 
-  /* remove class 'active' from all article links  */
+/* remove class 'active' from all article links  */
 
-  /* add class 'active' to the clicked link */
+const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* remove class 'active' from all articles */
+for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
 
-  /* get 'href' attribute from the clicked link */
+/* add class 'active' to the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+/* remove class 'active' from all articles */
 
-  /* add class 'active' to the correct article */
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+const activeArticles = document.querySelectorAll('.posts article.active');
+
+for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}
+
+/* get 'href' attribute from the clicked link */
+
+/* find the correct article using the selector (value of 'href' attribute) */
+
+/* add class 'active' to the correct article */
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+link.addEventListener('click', titleClickHandler);
+}
