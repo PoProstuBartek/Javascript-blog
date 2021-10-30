9f464636dfb56978d8e226ba09d48fe6f2785731
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2021-10-30 16:58:33 +0200

    Finished titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@ function titleClickHandler(){
   buttonTest.addEventListener('click', titleClickHandler); */
   
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
+    // console.log('Link was clicked!');
 
 
     /* [DONE] remove class 'active' from all article links  */
@@ -23,10 +24,10 @@ function titleClickHandler(event){
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
+    // console.log('clickedElement:', clickedElement);
 
     /* [DONE] remove class 'active' from all articles */
 
@@ -38,9 +39,18 @@ function titleClickHandler(event){
 
     /* get 'href' attribute from the clicked link */
 
+    const articleSelector = clickedElement.getAttribute("href");
+    // console.log(articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(articleSelector);
+    // console.log(targetArticle);
+
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
 }
     const links = document.querySelectorAll('.titles a');
 
