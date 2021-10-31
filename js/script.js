d0753dfc934aad19c9c8ac4b751ba9e0168a458d
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2021-10-31 10:11:26 +0100

    Clean up script comments

--- js/script.js
+++ js/script.js
@@ -31,8 +31,6 @@ const optArticleSelector = '.post',
     optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
-    
-    /* remove contents of titleList */
 
     const titleList = document.querySelector(optTitleListSelector);
     function clearTitleList(){
@@ -40,35 +38,20 @@ function generateTitleLinks(){
     }
     clearTitleList();
 
-    /* for each article */
-
     const articles = document.querySelectorAll(optArticleSelector);
     let html = '';
 
     for(let article of articles){
         article.classList.add('article');
 
-        /* get the article id */
-
         const articleId = article.getAttribute('id');
-
-        /* find the title element */
 
         const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        /* create HTML of the link */
-
         const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-        /* insert link into titleList */
-
-        // titleList.innerHTML = titleList.innerHTML + linkHTML; - opcja1 - zbyt obciąża stronę
-        // titleList.insertAdjacentHTML('beforeend', linkHTML); - opcja2
-
-        html = html + linkHTML; // opcja3
+        html = html + linkHTML; 
     }
-    
-    /* get the title from the title element */
 
     titleList.innerHTML = html;
 
